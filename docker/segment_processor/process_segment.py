844efaacb15e5d4a8bda2b2b4c5071959653af4d
salma-remyx <salma@remyx.ai>
2024-10-07 00:10:05 +0000

    updating w/ florence + SAM2

--- docker/segment_processor/process_segment.py
+++ docker/segment_processor/process_segment.py
@@ -1,47 +1,39 @@
 import os
-import cv2
 import pickle
 import argparse
 import numpy as np
 import pandas as pd
-from vqasynth.datasets.segment import CLIPSeg, SAM, sample_points_from_heatmap
+from vqasynth.datasets.segment import Flore, SAM2
 
-
-clipseg = CLIPSeg(model_name="CIDAS/clipseg-rd64-refined")
-sam = SAM(model_name="facebook/sam-vit-huge", device="cuda")
+florence = Florence()
+sam2 = SAM2()
 
 def segment_image_data(row):
     try:
-        preds = clipseg.run_inference(row["image"], row["captions"])
-
-        sampled_points = []
+        preds = florence.run_inference(row["image"])
         sam_masks = []
 
         original_size = row["image"].size
 
-        for idx in range(preds.shape[0]):
-            sampled_points.append(sample_points_from_heatmap(preds[idx][0], original_size, num_points=10))
+        for pred in preds:
+            bboxes = pred['bboxes']
 
-        for idx in range(preds.shape[0]):
-            mask_tensor = sam.run_inference_from_points(row["image"], [sampled_points[idx]])
-            mask = cv2.cvtColor(255 * mask_tensor[0].numpy().squeeze().transpose((1, 2, 0)).astype(np.uint8), cv2.COLOR_BGR2GRAY)
-            sam_masks.append(mask)
+            for bbox in bboxes:
+                mask_tensor = sam2.run_inference(row["image"], bbox)
+                mask = mask_tensor[0]
+                mask_uint8 = (mask.astype(np.uint8)) * 255
+                sam_masks.append(mask_uint8)
 
-        return sam_masks
+        return sam_masks, preds
     except:
-        return []
+        return [], []
 
 def main(output_dir):
     for filename in os.listdir(output_dir):
         if filename.endswith('.pkl'):
             pkl_path = os.path.join(output_dir, filename)
-            # Load the DataFrame from the .pkl file
             df = pd.read_pickle(pkl_path)
-
-            # Process each image and add the results to a new column
-            df['masks'] = df.apply(segment_image_data, axis=1)
-
-            # Save the updated DataFrame back to the .pkl file
+            df[['masks', 'bboxes']] = df.apply(lambda row: pd.Series(segment_image_data(row)), axis=1)
             df.to_pickle(pkl_path)
             print(f"Processed and updated {filename}")
 
--- vqasynth/datasets/segment.py
+++ vqasynth/datasets/segment.py
@@ -1,32 +1,71 @@
 import torch
 import numpy as np
 
-from transformers import SamModel, SamProcessor
-from transformers import AutoProcessor, CLIPSegForImageSegmentation
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+from transformers import AutoModelForCausalLM, AutoProcessor
 
-
-class CLIPSeg:
-    def __init__(self, model_name="CIDAS/clipseg-rd64-refined"):
-        self.clipseg_processor = AutoProcessor.from_pretrained(model_name)
-        self.clipseg_model = CLIPSegForImageSegmentation.from_pretrained(model_name)
-
-    def run_inference(self, image, text_descriptions):
-        inputs = self.clipseg_processor(text=text_descriptions, images=[image] * len(text_descriptions), padding=True, return_tensors="pt")
-        outputs = self.clipseg_model(**inputs)
-        logits = outputs.logits
-        return logits.detach().unsqueeze(1)
-
-class SAM:
-    def __init__(self, model_name="facebook/sam-vit-huge", device="cuda"):
+class FlorenceSeg:
+    def __init__(self, model_name="microsoft/Florence-2-large", device="cuda"):
         self.device = device
-        self.sam_model = SamModel.from_pretrained(model_name).to(self.device)
-        self.sam_processor = SamProcessor.from_pretrained(model_name)
+        self.torch_dtype = torch.float16
+        self.processor = AutoProcessor.from_pretrained(model_name, trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=self.torch_dtype, trust_remote_code=True).to(self.device)
 
-    def run_inference_from_points(self, image, points):
-        sam_inputs = self.sam_processor(image, input_points=points, return_tensors="pt").to(self.device)
-        with torch.no_grad():
-            sam_outputs = self.sam_model(**sam_inputs)
-        return self.sam_processor.image_processor.post_process_masks(sam_outputs.pred_masks.cpu(), sam_inputs["original_sizes"].cpu(), sam_inputs["reshaped_input_sizes"].cpu())
+    def run_inference(self, image):
+        final_answers = []
+        prompt = "<MORE_DETAILED_CAPTION>"
+        inputs = self.processor(text=prompt, images=image, return_tensors="pt").to(self.device, self.torch_dtype)
+
+        generated_ids = self.model.generate(
+            input_ids=inputs["input_ids"],
+            pixel_values=inputs["pixel_values"],
+            max_new_tokens=1024,
+            num_beams=3,
+            do_sample=False
+        )
+        generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+        parsed_answer = self.processor.post_process_generation(generated_text, task=prompt, image_size=(image.width, image.height))
+        captions = parsed_answer[task].split('.')
+        
+        for caption in captions:
+            if caption:
+                task = "<CAPTION_TO_PHRASE_GROUNDING>"
+                prompt = f"{task} {caption}"
+
+
+                url = "https://remyx.ai/assets/spatialvlm/warehouse_rgb.jpg?download=true"
+                image = Image.open("/content/pliers.png").convert("RGB") #requests.get(url, stream=True).raw)
+
+                inputs = processor(text=prompt, images=image, return_tensors="pt").to(device, torch_dtype)
+
+                generated_ids = model.generate(
+                    input_ids=inputs["input_ids"],
+                    pixel_values=inputs["pixel_values"],
+                    max_new_tokens=1024,
+                    num_beams=3,
+                    do_sample=False
+                )
+                generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+
+                parsed_answer = processor.post_process_generation(generated_text, task=task, image_size=(image.width, image.height))
+                final_answers.append(parsed_answer[task])
+
+        return final_answers
+
+class SAM2:
+    def __init__(self, model_name="facebook/sam2-hiera-large", device="cuda"):
+        self.device = device
+        self.sam2_model = SAM2ImagePredictor.from_pretrained(model_name, device=self.device)
+
+    def run_inference_from_points(self, image, input_box):
+        input_box = np.array(list(map(int, input_box['bboxes'][0])))
+        self.sam2_model.set_image(image)
+        masks, scores, _ = self.sam2_model.predict(
+            point_coords=None,
+            box=input_box[None, :],
+            multimask_output=False,
+        )
+        return masks.astype(bool)
 
 def find_medoid_and_closest_points(points, num_closest=5):
     """
@@ -70,6 +109,30 @@ def sample_points_from_heatmap(heatmap, original_size, num_points=5, percentile=
         y = width * y / w
         pts.append([y, x])
     return pts
+
+def sample_points_from_bbox(bbox, num_points=5):
+    """
+    Sample points from the given bounding box and find the medoid and closest points.
+
+    Parameters:
+    bbox (list or tuple): Bounding box coordinates [xmin, ymin, xmax, ymax].
+    num_points (int): Number of points to sample.
+
+    Returns:
+    list: A list of sampled points with coordinates [x, y].
+    """
+    xmin, ymin, xmax, ymax = bbox
+
+    # Sample random points within the bounding box
+    sampled_x = np.random.uniform(xmin, xmax, num_points)
+    sampled_y = np.random.uniform(ymin, ymax, num_points)
+    sampled_points = np.vstack((sampled_x, sampled_y)).T  # Shape (num_points, 2)
+
+    # Find the medoid and closest points
+    medoid, closest_points = find_medoid_and_closest_points(sampled_points)
+
+    # Return as a list of [x, y] coordinates
+    return [list(pt) for pt in closest_points]
 
 
 def apply_mask_to_image(image, mask):
