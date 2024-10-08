0315b8279856e5c6eace104db6395ee53372da36
salma-remyx <salma@remyx.ai>
2024-10-08 03:47:39 +0000

    produce masks, bboxes, and captions

--- docker/segment_processor/process_segment.py
+++ docker/segment_processor/process_segment.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import random
 import argparse
 import numpy as np
 import pandas as pd
@@ -8,38 +9,63 @@ from vqasynth.datasets.segment import Florence2, SAM2
 florence2 = Florence2()
 sam2 = SAM2()
 
+
 def segment_image_data(row):
     try:
         preds = florence2.run_inference(row["image"])
         sam_masks = []
+        final_bboxes = []
+        final_captions = []
 
         original_size = row["image"].size
 
+        object_counter = 1
         for pred in preds:
-            bboxes = pred['bboxes']
+            bboxes = pred.get("bboxes", [])
+            captions = pred.get("labels", [])
 
-            for bbox in bboxes:
-                mask_tensor = sam2.run_inference(row["image"], bbox)
+            if bboxes and captions and len(bboxes) == len(captions):
+                random_index = random.randint(0, len(bboxes) - 1)
+                selected_bbox = bboxes[random_index]
+                selected_caption = captions[random_index]
+
+                mask_tensor = sam2.run_inference(row["image"], selected_bbox)
                 mask = mask_tensor[0]
                 mask_uint8 = (mask.astype(np.uint8)) * 255
                 sam_masks.append(mask_uint8)
 
-        return sam_masks, preds
-    except:
-        return [], []
+                final_bboxes.append(selected_bbox)
+                final_captions.append(selected_caption)
+
+        return sam_masks, final_bboxes, final_captions
+    except Exception as e:
+        print(f"Error during segmentation: {str(e)}")
+        return [], [], []
+
 
 def main(output_dir):
     for filename in os.listdir(output_dir):
-        if filename.endswith('.pkl'):
+        if filename.endswith(".pkl"):
             pkl_path = os.path.join(output_dir, filename)
             df = pd.read_pickle(pkl_path)
-            df[['masks', 'bboxes']] = df.apply(lambda row: pd.Series(segment_image_data(row)), axis=1)
+            df[["masks", "bboxes", "captions"]] = pd.DataFrame(
+                df.apply(lambda row: segment_image_data(row), axis=1).tolist(),
+                index=df.index,
+            )
             df.to_pickle(pkl_path)
             print(f"Processed and updated {filename}")
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process images from .pkl files", add_help=True)
-    parser.add_argument("--output_dir", type=str, required=True, help="path to directory containing .pkl files")
+    parser = argparse.ArgumentParser(
+        description="Process images from .pkl files", add_help=True
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        required=True,
+        help="path to directory containing .pkl files",
+    )
     args = parser.parse_args()
 
     main(args.output_dir)
--- vqasynth/datasets/segment.py
+++ vqasynth/datasets/segment.py
@@ -4,62 +4,82 @@ import numpy as np
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 from transformers import AutoModelForCausalLM, AutoProcessor
 
+
 class Florence2:
     def __init__(self, model_name="microsoft/Florence-2-large", device="cuda"):
         self.device = device
         self.torch_dtype = torch.float16
-        self.processor = AutoProcessor.from_pretrained(model_name, trust_remote_code=True)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=self.torch_dtype, trust_remote_code=True).to(self.device)
+        self.processor = AutoProcessor.from_pretrained(
+            model_name, trust_remote_code=True
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name, torch_dtype=self.torch_dtype, trust_remote_code=True
+        ).to(self.device)
 
     def run_inference(self, image):
         final_answers = []
-        prompt = "<MORE_DETAILED_CAPTION>"
-        inputs = self.processor(text=prompt, images=image, return_tensors="pt").to(self.device, self.torch_dtype)
+        task = "<MORE_DETAILED_CAPTION>"
+        prompt = f"{task}"
+        inputs = self.processor(text=prompt, images=image, return_tensors="pt").to(
+            self.device, self.torch_dtype
+        )
 
         generated_ids = self.model.generate(
             input_ids=inputs["input_ids"],
             pixel_values=inputs["pixel_values"],
             max_new_tokens=1024,
             num_beams=3,
-            do_sample=False
+            do_sample=False,
         )
-        generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
-        parsed_answer = self.processor.post_process_generation(generated_text, task=prompt, image_size=(image.width, image.height))
-        captions = parsed_answer[task].split('.')
-        
+        generated_text = self.processor.batch_decode(
+            generated_ids, skip_special_tokens=False
+        )[0]
+        parsed_answer = self.processor.post_process_generation(
+            generated_text, task=prompt, image_size=(image.width, image.height)
+        )
+        captions = parsed_answer[task].split(".")
+
         for caption in captions:
             if caption:
                 task = "<CAPTION_TO_PHRASE_GROUNDING>"
                 prompt = f"{task} {caption}"
-                inputs = self.processor(text=prompt, images=image, return_tensors="pt").to(self.device, self.torch_dtype)
+                inputs = self.processor(
+                    text=prompt, images=image, return_tensors="pt"
+                ).to(self.device, self.torch_dtype)
 
                 generated_ids = self.model.generate(
                     input_ids=inputs["input_ids"],
                     pixel_values=inputs["pixel_values"],
                     max_new_tokens=1024,
                     num_beams=3,
-                    do_sample=False
+                    do_sample=False,
                 )
-                generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
-                parsed_answer = self.processor.post_process_generation(generated_text, task=task, image_size=(image.width, image.height))
+                generated_text = self.processor.batch_decode(
+                    generated_ids, skip_special_tokens=False
+                )[0]
+                parsed_answer = self.processor.post_process_generation(
+                    generated_text, task=task, image_size=(image.width, image.height)
+                )
                 final_answers.append(parsed_answer[task])
 
         return final_answers
 
+
 class SAM2:
     def __init__(self, model_name="facebook/sam2-hiera-large", device="cuda"):
         self.device = device
-        self.sam2_model = SAM2ImagePredictor.from_pretrained(model_name, device=self.device)
+        self.sam2_model = SAM2ImagePredictor.from_pretrained(
+            model_name, trust_remote_code=True, device=self.device
+        )
 
     def run_inference(self, image, input_box):
-        input_box = np.array(list(map(int, input_box['bboxes'][0])))
+        input_box = np.array(list(map(int, input_box)))
         self.sam2_model.set_image(image)
         masks, scores, _ = self.sam2_model.predict(
-            point_coords=None,
-            box=input_box[None, :],
-            multimask_output=False,
+            point_coords=None, box=input_box[None, :], multimask_output=False
         )
         return masks.astype(bool)
+
 
 def find_medoid_and_closest_points(points, num_closest=5):
     """
@@ -73,13 +93,16 @@ def find_medoid_and_closest_points(points, num_closest=5):
     np.array: The medoid point.
     np.array: The closest points to the medoid.
     """
-    distances = np.sqrt(((points[:, np.newaxis, :] - points[np.newaxis, :, :]) ** 2).sum(axis=-1))
+    distances = np.sqrt(
+        ((points[:, np.newaxis, :] - points[np.newaxis, :, :]) ** 2).sum(axis=-1)
+    )
     distance_sums = distances.sum(axis=1)
     medoid_idx = np.argmin(distance_sums)
     medoid = points[medoid_idx]
     sorted_indices = np.argsort(distances[medoid_idx])
-    closest_indices = sorted_indices[1:num_closest + 1]
+    closest_indices = sorted_indices[1 : num_closest + 1]
     return medoid, points[closest_indices]
+
 
 def sample_points_from_heatmap(heatmap, original_size, num_points=5, percentile=0.95):
     """
@@ -92,7 +115,9 @@ def sample_points_from_heatmap(heatmap, original_size, num_points=5, percentile=
 
     attn = torch.sigmoid(heatmap)
     w = attn.shape[0]
-    sampled_indices = torch.multinomial(torch.tensor(probabilities.ravel()), num_points, replacement=True)
+    sampled_indices = torch.multinomial(
+        torch.tensor(probabilities.ravel()), num_points, replacement=True
+    )
 
     sampled_coords = np.array(np.unravel_index(sampled_indices, attn.shape)).T
     medoid, sampled_coords = find_medoid_and_closest_points(sampled_coords)
@@ -103,6 +128,7 @@ def sample_points_from_heatmap(heatmap, original_size, num_points=5, percentile=
         y = width * y / w
         pts.append([y, x])
     return pts
+
 
 def apply_mask_to_image(image, mask):
     """
