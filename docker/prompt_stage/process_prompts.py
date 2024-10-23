710efa03c18c8faff7289a218ad26e4428ab42a5
Salma Mayorquin <salma@remyx.ai>
2024-10-23 08:32:18 -0700

    improving distance scaling and proc (#23)
    Co-authored-by: smellslikeml <terry.j.rodriguez@gmail.com>

--- docker/prompt_stage/process_prompts.py
+++ docker/prompt_stage/process_prompts.py
@@ -22,12 +22,9 @@ def save_and_push_datasets(dataset, output_dir, target_repo_name, images, datalo
         images: The column name for images.
         dataloader: Dataloader instance to handle saving and pushing datasets.
     """
-    # Save the full dataset to CSV and push it to the hub
-    dataset["train"].to_csv(os.path.join(output_dir, f"{target_repo_name}_full.csv"))
     dataloader.save_to_disk(dataset)
     dataloader.push_to_hub(dataset, f"{target_repo_name}_full")
 
-    # Select only the images and messages columns and push to the hub
     final_dataset = dataset.select_columns([images, "messages"])
     dataloader.push_to_hub(final_dataset, target_repo_name)
 
@@ -35,20 +32,16 @@ def main(output_dir, source_repo_id, target_repo_name, images):
     prompt_generator = PromptGenerator()
     dataloader = Dataloader(output_dir)
 
-    # Load dataset from source repository
     dataset = dataloader.load_dataset(source_repo_id)
-
-    # Apply transformation using the prompt generator, supporting batching
     dataset = dataset.map(
         prompt_generator.apply_transform,
         batched=True,
         batch_size=32
     )
 
-    # Filter out rows with None values
+    dataset = dataset.filter(lambda x: [len(msg) > 0 for msg in x['messages']], batched=True, batch_size=32)
     dataset = dataset.filter(filter_null, batched=True, batch_size=32)
 
-    # Save and push the full dataset and the dataset with selected columns
     save_and_push_datasets(dataset, output_dir, target_repo_name, images, dataloader)
 
     print(f"Processed and updated dataset with formatted messages.")
